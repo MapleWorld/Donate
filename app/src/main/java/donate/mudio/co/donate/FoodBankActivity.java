109c3525f8107c8f73418c8c9d692e48e9d43c42
unknown <ou.ye.canada@hotmail.com>
2015-09-19 23:29:56 -0400

    Added style to xml and add code

--- app/src/main/java/donate/mudio/co/donate/FoodBankActivity.java
+++ app/src/main/java/donate/mudio/co/donate/FoodBankActivity.java
@@ -1,12 +1,16 @@
 package donate.mudio.co.donate;
 
-import android.content.Intent;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class FoodBankActivity extends AppCompatActivity {
 
@@ -15,11 +19,25 @@ public class FoodBankActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_bank);
 
-        Intent intentN = getIntent();
-        String foodBankName = intentN.getStringExtra("foodBankName");
         // Send the food bank name to server to get the info
         // Show the info in the page
-        ((TextView) findViewById(R.id.food_bank_info)).setText(foodBankName + " Other Random Data");
+
+        findViewById(R.id.donate_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DonateFragment new_donate_dialog = new DonateFragment();
+                FragmentManager fm = getSupportFragmentManager();
+                new_donate_dialog.show(fm, "donate_fragment_dialog");
+                // Send data to server
+                EditText donation_name = (EditText) findViewById(R.id.donation_name);
+                EditText donation_weight = (EditText) findViewById(R.id.donation_weight);
+                EditText donation_number = (EditText) findViewById(R.id.donation_number);
+                String donation_name_string = donation_name.getText().toString();
+                String donation_weight_string = donation_weight.getText().toString();
+                String donation_number_string = donation_number.getText().toString();
+                // Send these data to server
+            }
+        });
     }
 
     @Override
@@ -42,5 +60,30 @@ public class FoodBankActivity extends AppCompatActivity {
             new_location_dialog.show(fm, "location_fragment_dialog");
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public static class DonateFragment extends DialogFragment {
+
+        public DonateFragment() {
+            // Empty constructor required for DialogFragment
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.donate_fragment_dialog, container);
+            view.findViewById(R.id.send_donate_button)
+                    .setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            getDialog().dismiss();
+                            // Refresh the current page
+                            Activity activity = getActivity();
+                            activity.finish();
+                            startActivity(activity.getIntent());
+                        }
+                    });
+            return view;
+        }
     }
 }
